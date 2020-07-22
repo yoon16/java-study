ff721e3fab9d388de46b938ff727a0ba5d105eb5
kickscar <kickscar@gmail.com>
2020-07-22 17:00:18 +0900

    TV 구현 완

--- chapter03/src/main/java/com/bit2020/chapter03/tv/TV.java
+++ chapter03/src/main/java/com/bit2020/chapter03/tv/TV.java
@@ -5,6 +5,52 @@ public class TV {
 	private int volume;
 	private boolean power;
 	
+	public TV(int channel, int volume, boolean power) {
+		this.channel = channel;
+		this.volume = volume;
+		this.power = power;
+	}
+	
+	public void power(boolean on) {
+		power = on;
+	}
+	
+	public void channel(int channel) {
+		if(!power) {
+			return;
+		}
+		
+		if(channel < 1) {
+			channel = 255;
+		} else if( channel > 255) {
+			channel = 1;
+		}
+		
+		this.channel = channel;
+	}
+	
+	public void channel(boolean up) {
+		channel(channel + (up ? 1 : -1));
+	}
+
+	public void volume(int volume) {
+		if(!power) {
+			return;
+		}
+		
+		if(volume < 1) {
+			volume = 255;
+		} else if( volume > 255) {
+			volume = 1;
+		}
+		
+		this.volume = volume;
+	}
+	
+	public void volume(boolean up) {
+		volume(volume + (up ? 1 : -1));
+	}
+	
 	public void status() {
 		System.out.println(
 			"TV[power=" + power + 
